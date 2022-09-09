3b23d385c908a2c91f3b684726fdfc4e6a147a03
Vitalii <volfram@ukr.net>
2022-09-10 01:30:41 +0300

    add second & third section

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,16 +1,31 @@
-const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      close = document.querySelector('.menu__close'),
-      overlay = document.querySelector('.menu__overlay');
+const hamburger = document.querySelector(".hamburger"),
+  menu = document.querySelector(".menu"),
+  close = document.querySelector(".menu__close"),
+  overlay = document.querySelector(".menu__overlay");
 
-hamburger.addEventListener('click', () => {
-  menu.classList.add('active');
+hamburger.addEventListener("click", () => {
+  menu.classList.add("active");
 });
 
-close.addEventListener('click', () => {
-  menu.classList.remove('active');
+close.addEventListener("click", () => {
+  menu.classList.remove("active");
 });
 
-overlay.addEventListener('click', () => {
-  menu.classList.remove('active');
+overlay.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
+
+function percents() {
+  const counter = document.querySelectorAll(".skills__rating"),
+    line = document.querySelectorAll(".skills__visualisation-graph span");
+
+  counter.forEach((item, i) => {
+    line[i].style.width = item.innerHTML;
+  });
+}
+
+window.addEventListener("scroll", function (e) {
+  if (this.scrollY > 2700) {
+    percents();
+  }
 });
